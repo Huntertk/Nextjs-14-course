a12d1930596be06bcadb4a323f1a9fe61a3efb83
Huntertk <taufik.khan65@gmail.com>
2024-01-09 01:44:17 +0530

    Baisc design with css and making responsive

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,30 +1,55 @@
-import Link from 'next/link'
+"use client"
+import { useState } from 'react';
+import styles from './links.module.css'
+import NavLinks from './navlinks/NavLinks';
+
+
+const links = [
+    {
+        title:"Homepage",
+        path:"/"
+    },
+    {
+        title:"About",
+        path:"/about"
+    },
+    {
+        title:"Contact",
+        path:"/contact"
+    },
+    {
+        title:"Blog",
+        path:"/blog"
+    },
+];
 
 const Links = () => {
-    const links = [
-        {
-            title:"Homepage",
-            path:"/"
-        },
-        {
-            title:"About",
-            path:"/about"
-        },
-        {
-            title:"Contact",
-            path:"/contact"
-        },
-        {
-            title:"Blog",
-            path:"/blog"
-        },
-    ];
+    const [open, setOpen] = useState(false)
 
-
+    //Temp
+    const session = true;
+    const isAdmin = true;
   return (
-
-    <div>
-        {links.map((link, i) => <Link key={i} href={link.path}>{link.title}</Link>)}
+    <div className={styles.container}>
+        <div className={styles.links}>
+            {links.map((link, i) => <NavLinks item={link} key={i} />)}
+            {session ? (
+                <>
+                {
+                    isAdmin && ( <NavLinks item={{title:"Admin", path:"/admin"}} />)        
+                }
+                <button className={styles.logout}>Logout</button>
+                </>
+            ) : (
+                <NavLinks item={{title:"Login", path:"/login"}} />
+                )}
+        </div>
+        <button className={styles.menuButton} onClick={() => setOpen(prev => !prev)}>Menu</button>
+        {
+            open && <div className={styles.mobileLinks}>
+                {links.map((link, i) => <NavLinks item={link} key={i} />)}
+            </div>
+        }
     </div>
   )
 }
