7150d24d273c2e23f9b98f12ae727d7dc52fdd04
Huntertk <taufik.khan65@gmail.com>
2024-01-09 13:39:48 +0530

    Contact Page Design Completed

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import styles from './contact.module.css'
+import Image from 'next/image'
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src={"/contact.png"} alt='contact image' fill priority className={styles.img}  />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder='Name and Surname' />
+          <input type="Email" placeholder='Email' />
+          <input type="number" placeholder='Phone (Optional)' />
+          <textarea name="" id="" cols="30" rows="10" placeholder='message'></textarea>
+          <button type='submit' className={styles.button}>Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
