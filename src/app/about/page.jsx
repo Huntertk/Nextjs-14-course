07eee2b5b5209d9c739989dc37c537754bbbb55e
Huntertk <taufik.khan65@gmail.com>
2024-01-09 11:44:02 +0530

    next js image components

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import Image from 'next/image'
+import styles from './about.module.css'
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div>
+        <Image src="/about.png" alt="image" width={500} height={500} priority />
+      <div className={styles.imgContainer}>
+        <Image 
+          src="https://images.pexels.com/photos/18013547/pexels-photo-18013547/free-photo-of-a-woman-in-a-green-dress-sitting-on-a-wooden-bench.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+          alt="image" 
+          fill 
+        />
+      </div>
+    </div>
   )
 }
 
