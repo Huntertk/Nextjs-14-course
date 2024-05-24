b8e2ce7fa94db2fa24add08cc81a9191c13b8121
Huntertk <taufik.khan65@gmail.com>
2024-05-24 23:23:30 +0530

    Addng Route using file based routing system

--- nextjsessential/app/page.js
+++ nextjsessential/app/page.js
@@ -1,9 +1,15 @@
-export default function Home() {
+import Link from "next/link";
+
+const Home = () => {
+  console.log("hello");
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <Link href={'/about'}>About Page</Link>
     </main>
-  );
+  )
 }
+
+export default Home
