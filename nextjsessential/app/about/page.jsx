cfe364b59cc5aefeee160ed7bf69363a3f92a84b
Huntertk <taufik.khan65@gmail.com>
2024-05-24 23:32:03 +0530

    Navigating Between Pages

--- nextjsessential/app/about/page.jsx
+++ nextjsessential/app/about/page.jsx
@@ -1,9 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const AboutPage = () => {
   return (
     <main>
         <h1>About Page</h1>
+        <p><Link href={'/'}>Home</Link></p>
     </main>
   )
 }
--- nextjsessential/app/page.js
+++ nextjsessential/app/page.js
@@ -7,7 +7,7 @@ const Home = () => {
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
-      <Link href={'/about'}>About Page</Link>
+      <p><Link href={'/about'}>About Page</Link></p>
     </main>
   )
 }
