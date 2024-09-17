605419e325650a3d3bc138833985d4d24e87faae
Huntertk <taufik.khan65@gmail.com>
2024-09-17 23:13:28 +0530

    favicon added conponents created importing with alias

---
+++ nextjsessential/app/about/page.js
@@ -0,0 +1,12 @@
+import Link from "next/link"
+
+const AboutPage = () => {
+  return (
+    <main>
+        <h1>About</h1>
+        <p><Link href="/">Home</Link></p>
+    </main>
+  )
+}
+
+export default AboutPage
---
+++ nextjsessential/components/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <><img src="/logo.png" alt="A server surrounded by magic sparkles." /></>
+  )
+}
+
+export default Header
