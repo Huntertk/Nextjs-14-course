0653f7dc479812145f3a40cfa80c5d86fbf58666
Huntertk <taufik.khan65@gmail.com>
2024-09-17 23:32:23 +0530

    food ordering app setup

--- nextjsessential/app/page.js
+++ nextjsessential/app/page.js
@@ -1,15 +1,9 @@
-import Link from "next/link";
-import Header from "@/components/header";
-
-const Home = () => {
+export default function Home() {
   return (
     <main>
-      <Header />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
-      <p><Link href={'/about'}>About Page</Link></p>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
-  )
+  );
 }
-
-export default Home
